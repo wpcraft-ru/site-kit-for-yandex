42d0c4827de45bb6c7d6c69fae9316dfdd89adb7
Anatolii Iumashev <anatolii.iumashev@gmail.com>
2026-03-14 15:26:55 +0300

    Add Yandex Metrika and Yandex Webmaster tools pages under the Tools menu

--- includes/YandexMetrika.php
+++ includes/YandexMetrika.php
@@ -19,6 +19,56 @@ class YandexMetrika
     public static function init()
     {
         add_action('admin_init', [self::class, 'registerSettingsSection']);
+        add_action('admin_menu', [self::class, 'addToolsPage']);
+    }
+
+    /**
+     * Add Yandex Metrika page under Tools menu.
+     *
+     * @return void
+     */
+    public static function addToolsPage()
+    {
+        add_management_page(
+            __('Yandex Metrika', 'site-kit-for-yandex'),
+            __('Yandex Metrika', 'site-kit-for-yandex'),
+            'manage_options',
+            'site-kit-for-yandex-metrika',
+            [self::class, 'renderToolsPage']
+        );
+    }
+
+    /**
+     * Render Yandex Metrika tools page.
+     *
+     * @return void
+     */
+    public static function renderToolsPage()
+    {
+        ?>
+        <div class="wrap">
+            <h1><?php echo esc_html__('Yandex Metrika', 'site-kit-for-yandex'); ?></h1>
+            <p>
+                <?php
+                printf(
+                    '%1$s <a href="%2$s" target="_blank" rel="noopener noreferrer">%3$s</a>.',
+                    esc_html__('Просмотр информации о метрике:', 'site-kit-for-yandex'),
+                    esc_url('https://metrika.yandex.ru/'),
+                    esc_html__('metrika.yandex.ru', 'site-kit-for-yandex')
+                );
+                ?>
+            </p>
+            <p>
+                <?php
+                printf(
+                    '<a href="%1$s">%2$s</a>.',
+                    esc_url(admin_url('options-general.php?page=site-kit-for-yandex')),
+                    esc_html__('Настройки плагина', 'site-kit-for-yandex')
+                );
+                ?>
+            </p>
+        </div>
+        <?php
     }
 
     /**
@@ -43,9 +93,12 @@ class YandexMetrika
      */
     public static function renderSectionText()
     {
-
-        //Просмотр метрики по адресу https://metrika.yandex.ru/
-
+        printf(
+            '<p>%1$s <a href="%2$s">%3$s</a>.</p>',
+            esc_html__('Сводка по метрике доступна на странице', 'site-kit-for-yandex'),
+            esc_url(admin_url('tools.php?page=site-kit-for-yandex-metrika')),
+            esc_html__('Yandex Metrika Tools', 'site-kit-for-yandex')
+        );
 
         printf(
             '<p>%1$s <a href="%2$s" target="_blank" rel="noopener noreferrer">%3$s</a></p>',
--- includes/YandexWebmaster.php
+++ includes/YandexWebmaster.php
@@ -19,6 +19,57 @@ class YandexWebmaster
     public static function init()
     {
         add_action('admin_init', [self::class, 'registerSettingsSection']);
+        add_action('admin_menu', [self::class, 'addToolsPage']);
+    }
+
+    /**
+     * Add Yandex Webmaster page under Tools menu.
+     *
+     * @return void
+     */
+    public static function addToolsPage()
+    {
+        add_management_page(
+            __('Yandex Webmaster', 'site-kit-for-yandex'),
+            __('Yandex Webmaster', 'site-kit-for-yandex'),
+            'manage_options',
+            'site-kit-for-yandex-webmaster',
+            [self::class, 'renderToolsPage']
+        );
+    }
+
+    /**
+     * Render Yandex Webmaster tools page.
+     *
+     * @return void
+     */
+    public static function renderToolsPage()
+    {
+        ?>
+        <div class="wrap">
+            <h1><?php echo esc_html__('Yandex Webmaster', 'site-kit-for-yandex'); ?></h1>
+            <p>
+                <?php
+                printf(
+                    '%1$s <a href="%2$s" target="_blank" rel="noopener noreferrer">%3$s</a>.',
+                    esc_html__('Просмотр информации о сайте в целом:', 'site-kit-for-yandex'),
+                    esc_url('https://webmaster.yandex.ru/'),
+                    esc_html__('webmaster.yandex.ru', 'site-kit-for-yandex')
+                );
+                ?>
+            </p>
+            <p>
+                <?php
+                printf(
+                    '<a href="%1$s">%2$s</a>.',
+                    esc_url(admin_url('options-general.php?page=site-kit-for-yandex')),
+                    esc_html__('Настройки плагина', 'site-kit-for-yandex')
+                );
+                ?>
+            </p>
+
+        </div>
+        <?php
     }
 
     /**
@@ -43,6 +94,15 @@ class YandexWebmaster
      */
     public static function renderSectionText()
     {
+
+        // Сводка /wp-admin/tools.php?page=site-kit-for-yandex-webmaster
+        printf(
+            '<p>%1$s <a href="%2$s">%3$s</a>.</p>',
+            esc_html__('Сводка по сайту доступна на странице', 'site-kit-for-yandex'),
+            esc_url(admin_url('tools.php?page=site-kit-for-yandex-webmaster')),
+            esc_html__('Yandex Webmaster Tools', 'site-kit-for-yandex')
+        );
+
         printf(
             '<p>%1$s <a href="%2$s" target="_blank" rel="noopener noreferrer">%3$s</a></p>',
             esc_html__('Подключение сайта и просмотр данных Яндекс.Вебмастера осуществляется через сайт', 'site-kit-for-yandex'),
