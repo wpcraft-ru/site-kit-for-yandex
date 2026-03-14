c9ded3429b66c27f29350a259c573e57fd268c6c
Anatolii Iumashev <anatolii.iumashev@gmail.com>
2026-03-14 14:14:27 +0300

    Refactor Settings class to improve option management methods

--- includes/Settings.php
+++ includes/Settings.php
@@ -9,6 +9,19 @@ final class Settings
         add_action('admin_menu', [self::class, 'addSettingsPage']);
     }
 
+    public function get($key)
+    {
+        $options = get_option('site_kit_for_yandex_options', []);
+        return isset($options[$key]) ? $options[$key] : null;
+    }
+
+    public function set($key, $value)
+    {
+        $options = get_option('site_kit_for_yandex_options', []);
+        $options[$key] = $value;
+        update_option('site_kit_for_yandex_options', $options);
+    }
+
     public static function addSettingsPage()
     {
         add_options_page(
@@ -18,35 +31,21 @@ final class Settings
             'site-kit-for-yandex',
             function () {
                 ?>
-            <div class="wrap">
-                <h1>Site Kit for Yandex Settings</h1>
-                <p>Configure your Yandex services integration here.</p>
-                <form method="post" action="options.php">
-                    <?php
-                        settings_fields('site_kit_for_yandex_options');
-                        do_settings_sections('site-kit-for-yandex');
-                        submit_button();
-                        ?>
-                </form>
-            </div>
-            <?php
+                        <div class="wrap">
+                            <h1>Site Kit for Yandex Settings</h1>
+                            <p>Configure your Yandex services integration here.</p>
+                            <form method="post" action="options.php">
+                                <?php
+                                    settings_fields('site_kit_for_yandex_options');
+                                    do_settings_sections('site-kit-for-yandex');
+                                    submit_button();
+                                    ?>
+                            </form>
+                        </div>
+                        <?php
             }
         );
 
         register_setting('site_kit_for_yandex_options', 'site_kit_for_yandex_options');
-    }
-
-    public function get($key)
-    {
-        $options = get_option('site_kit_for_yandex_options', []);
-        return isset($options[$key]) ? $options[$key] : null;
-    }
-
-    //set
-    public function set($key, $value)
-    {
-        $options = get_option('site_kit_for_yandex_options', []);
-        $options[$key] = $value;
-        update_option('site_kit_for_yandex_options', $options);
     }
 }
