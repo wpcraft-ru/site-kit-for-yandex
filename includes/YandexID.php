1a7cff1398a44f5d5ddfcbcde0bd64569b3e1f28
Anatolii Iumashev <anatolii.iumashev@gmail.com>
2026-03-27 20:37:02 +0300

    Update renderSectionText method to include authorization information for Yandex ID using Socialify plugin

--- includes/YandexID.php
+++ includes/YandexID.php
@@ -23,7 +23,15 @@ class YandexID
 
     public static function renderSectionText()
     {
-      //Яндекс ID — это единая учётная запись для доступа к сервисам Яндекса и партнёрским сайтам и приложениям с поддержкой этой авторизации; она позволяет безопасно входить в аккаунты, управлять личными данными, платёжными реквизитами и настройками, а также упрощает процесс авторизации — без необходимости запоминать множество логинов и паролей.
+        //Добавьте авторизацию через проверенный плагин https://wpcraft.ru/wordpress/plugins/socialify
+
+        printf(
+            '<p>%1$s <a href="%2$s" target="_blank" rel="noopener noreferrer">%3$s</a>.</p>',
+            esc_html__('Для авторизации через Яндекс ID можно использовать проверенный плагин Socialify авторизации, который поддерживает Яндекс ID.', 'site-kit-for-yandex'),
+            esc_url('https://wpcraft.ru/wordpress/plugins/socialify'),
+            esc_html__('Подробнее', 'site-kit-for-yandex')
+        );
+        
 
         printf(
             '<p>%1$s <a href="%2$s" target="_blank" rel="noopener noreferrer">%3$s</a>.</p>',
