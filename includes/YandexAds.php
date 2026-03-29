b50bd524bc40bee509d210482db8fa1fddad2d41
Anatolii Iumashev <anatolii.iumashev@gmail.com>
2026-03-29 17:07:14 +0300

    Swap content in renderSectionText method for Yandex Ads integration

--- includes/YandexAds.php
+++ includes/YandexAds.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 namespace SiteKitForYandex;
 
@@ -25,16 +25,16 @@ class YandexAds
     {
         printf(
             '<p>%1$s <a href="%2$s" target="_blank" rel="noopener noreferrer">%3$s</a>.</p>',
-            esc_html__('Размещайте рекламу на вашем сайте через Рекламную сеть Яндекса и зарабатывайте на трафике. Подключите площадку, настройте рекламные блоки и отслеживайте доход в удобной статистике.', 'site-kit-for-yandex'),
-            esc_url('https://yandex.ru/adv/publishers/monetization'),
-            esc_html__('Узнать больше о размещении рекламы на сайте', 'site-kit-for-yandex')
+            esc_html__('Плагин от команды WPCraft для удобного размещения рекламы на сайте:', 'site-kit-for-yandex'),
+            esc_url('https://wpcraft.ru/catalog/ads-media-planner'),
+            esc_html__('Ads Media Planner', 'site-kit-for-yandex')
         );
 
         printf(
             '<p>%1$s <a href="%2$s" target="_blank" rel="noopener noreferrer">%3$s</a>.</p>',
-            esc_html__('Плагин от команды WPCraft для удобного размещения рекламы на сайте:', 'site-kit-for-yandex'),
-            esc_url('https://wpcraft.ru/catalog/ads-media-planner'),
-            esc_html__('Ads Media Planner', 'site-kit-for-yandex')
+            esc_html__('Размещайте рекламу на вашем сайте через Рекламную сеть Яндекса и зарабатывайте на трафике. Подключите площадку, настройте рекламные блоки и отслеживайте доход в удобной статистике.', 'site-kit-for-yandex'),
+            esc_url('https://yandex.ru/adv/publishers/monetization'),
+            esc_html__('Узнать больше о размещении рекламы на сайте', 'site-kit-for-yandex')
         );
     }
 }
